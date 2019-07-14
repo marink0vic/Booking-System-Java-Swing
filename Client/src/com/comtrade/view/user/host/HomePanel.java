c7cd06bc57cab658e78272559e3c27456d92df1e
marink0vic <salamanka1985@gmail.com>
2019-07-14 21:32:35 +0200

    Add elements on user home panel

--- Client/src/com/comtrade/view/user/host/HomePanel.java
+++ Client/src/com/comtrade/view/user/host/HomePanel.java
@@ -16,11 +16,15 @@ import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
 import com.comtrade.domain.Address;
 import com.comtrade.domain.PropertyImage;
 import com.comtrade.domain.RoomType;
 import com.comtrade.dto.PropertyWrapper;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 
 public class HomePanel extends JPanel {
 
@@ -33,6 +37,10 @@ public class HomePanel extends JPanel {
 	private DefaultTableModel dtm = new DefaultTableModel();
 	private PropertyWrapper propertyOwner;
 	private List<PropertyImage> propertyImages;
+	//--galery images
+	private JLabel lblImage1;
+	private JLabel lblImage2;
+	private JLabel lblImage3;
 	
 	public HomePanel(PropertyWrapper propertyOwner) {
 		this.propertyOwner = propertyOwner;
@@ -47,14 +55,14 @@ public class HomePanel extends JPanel {
 		
 		lblPropertyName = new JLabel("");
 		lblPropertyName.setForeground(new Color(71, 71, 71));
-		lblPropertyName.setFont(new Font("Dialog", Font.BOLD, 18));
+		lblPropertyName.setFont(new Font("Dialog", Font.BOLD, 22));
 		lblPropertyName.setBorder(null);
 		lblPropertyName.setBounds(168, 71, 308, 37);
 		this.add(lblPropertyName);
 		
 		lblPropertyAddress = new JLabel("Propery Address");
 		lblPropertyAddress.setForeground(new Color(71, 71, 71));
-		lblPropertyAddress.setFont(new Font("Dialog", Font.PLAIN, 16));
+		lblPropertyAddress.setFont(new Font("Dialog", Font.PLAIN, 20));
 		lblPropertyAddress.setBorder(null);
 		lblPropertyAddress.setBounds(168, 107, 570, 37);
 		this.add(lblPropertyAddress);
@@ -65,51 +73,78 @@ public class HomePanel extends JPanel {
 		this.add(lblSmallImage);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(25, 207, 670, 224);
+		scrollPane.setBounds(25, 207, 749, 224);
 		this.add(scrollPane);
 		
 		table = new JTable(dtm);
+		table.setForeground(new Color(71, 71, 71));
+		table.setFont(new Font("Dialog", Font.BOLD, 17));
+		table.setRowHeight(30);
+		JTableHeader header = table.getTableHeader();
+		header.setFont(new Font("Dialog", Font.BOLD, 20));
 		scrollPane.setViewportView(table);
 		Object[] object = {"Room type", "Number of rooms", "Price per night"};
 		dtm.addColumn(object[0]);
 		dtm.addColumn(object[1]);
 		dtm.addColumn(object[2]);
 		
-		JLabel arrowLeft = new JLabel("");
-		arrowLeft.setHorizontalAlignment(SwingConstants.LEFT);
-		arrowLeft.setBorder(null);
-		arrowLeft.setBounds(25, 513, 56, 44);
-		Icon iconLeftArrow = new ImageIcon("./resources/icons/arrow-left.png");
-		arrowLeft.setIcon(iconLeftArrow);
-		this.add(arrowLeft);
-		
-		JLabel arrowRight = new JLabel("");
-		arrowRight.setHorizontalAlignment(SwingConstants.RIGHT);
-		arrowRight.setBorder(null);
-		arrowRight.setBounds(639, 513, 56, 44);
-		Icon iconRightArrow = new ImageIcon("./resources/icons/arrow-right.png");
-		arrowRight.setIcon(iconRightArrow);
-		this.add(arrowRight);
-		
-		JLabel lblImage1 = new JLabel("");
+		lblImage1 = new JLabel("");
 		lblImage1.setBorder(null);
-		lblImage1.setBounds(93, 467, 152, 152);
+		lblImage1.setBounds(26, 467, 152, 152);
 		this.add(lblImage1);
 		
-		JLabel lblImage2 = new JLabel("");
+		lblImage2 = new JLabel("");
 		lblImage2.setBorder(null);
-		lblImage2.setBounds(281, 467, 152, 152);
+		lblImage2.setBounds(227, 467, 152, 152);
 		this.add(lblImage2);
 		
-		JLabel lblImage3 = new JLabel("");
+		lblImage3 = new JLabel("");
 		lblImage3.setBorder(null);
-		lblImage3.setBounds(475, 467, 152, 152);
+		lblImage3.setBounds(430, 467, 152, 152);
 		this.add(lblImage3);
+		
+		JButton btnImages = new JButton("Add Images");
+		btnImages.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnImages.setBounds(647, 503, 129, 84);
+			}
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				btnImages.setBounds(649, 505, 125, 79);
+			}
+		});
+		btnImages.setBounds(649, 505, 125, 79);
+		btnImages.setForeground(Color.WHITE);
+		btnImages.setFont(new Font("Dialog", Font.BOLD, 15));
+		btnImages.setBorder(null);
+		btnImages.setBackground(new Color(9, 121, 186));
+		add(btnImages);
 		
 		addLabelsValues();
 		fillRoomTypeTable();
+		fillGallerySlider();
 	}
 	
+	private void fillGallerySlider() {
+		
+		if (propertyImages.get(0) != null) {
+			lblImage1.setIcon(setImageIcon(propertyImages.get(0).getImage()));
+		}
+		if (propertyImages.get(1) != null) {
+			lblImage2.setIcon(setImageIcon(propertyImages.get(1).getImage()));
+		}
+		if (propertyImages.get(2) != null) {
+			lblImage3.setIcon(setImageIcon(propertyImages.get(2).getImage()));
+		}
+	}
+	
+	private Icon setImageIcon(String image) {
+		File f = new File(image);
+		Icon icon = resizeImage(f, lblImage1.getWidth(), lblImage1.getHeight());
+		return icon;
+	}
+
 	private void addLabelsValues() {
 		lblPropertyName.setText(propertyOwner.getProperty().getName());
 		Address a = propertyOwner.getAddress();
@@ -137,5 +172,4 @@ public class HomePanel extends JPanel {
 			dtm.addRow(new Object[] {type.getRoomType(), type.getNumberOfRooms(), type.getPricePerNight()});
 		}
 	}
-
 }
